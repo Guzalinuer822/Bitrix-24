f63dfc0b7539d37f56a5bfc2f02145b4e55bf9c0
Guzalinuer822 <57195528+Guzalinuer822@users.noreply.github.com>
2019-12-09 19:18:22 +0100

    added return method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -3,11 +3,11 @@ package tests;
 public class task {
   
   public static void main(String[] args){
-    
-    System.out.println("hello world");
-    
+    System.out.println("hello world"); 
     System.out.println("See you in Intellij");
   
   }
-
+public String hello(){
+  return "hello from GitHub";
+}
 }
