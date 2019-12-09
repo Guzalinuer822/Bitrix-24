043c8e775d1f0bec70948e413eba3e5546aecead
guzal <myEmail@Gmail.com>
2019-12-09 19:15:43 +0100

    updated task again added void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -3,13 +3,12 @@ package tests;
 public class task {
   
   public static void main(String[] args){
-    
     System.out.println("hello world");
-    
     System.out.println("See you in Intellij");
   
   }
-
-
+  public void hi(){
+    System.out.println("hi from IntelliJ");
+}
 
 }
