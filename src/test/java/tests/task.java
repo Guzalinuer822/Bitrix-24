e8f83c0d5e7ee3b128451a2cbd860516a8174269
Guzalinuer822 <57195528+Guzalinuer822@users.noreply.github.com>
2019-12-09 18:37:51 +0100

    updated task class

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,5 +1,13 @@
 package tests;
 
 public class task {
+  
+  public static void main(String[] args){
+    
+    System.out.println("hello world");
+    
+    System.out.println("See you in Intellij");
+  
+  }
 
 }
